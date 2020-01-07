fc28c2064f62010e06ca88f3072e3511ae505961
Wan Mohd Adzha <wmadzha11@hotmail.com>
2020-01-07 12:01:22 +0800

    Init Entity Framework

---
+++ DaniaDaisy.EntityFramework/Products.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace DaniaDaisy.EntityFramework
+{
+    public partial class Products
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public Boolean? IsOnline { get; set; }
+    }
+
+    public static partial class ModelBuilders
+    {
+        public static void SetupProductEntity(this ModelBuilder builder)
+        {
+            builder.Entity<Products>(entity =>
+            {
+                entity.HasKey(e => e.ProductId);
+
+                entity.Property(e => e.ProductId).ValueGeneratedNever();
+
+                entity.Property(e => e.ProductCategory).HasMaxLength(255);
+
+                entity.Property(e => e.ProductName).HasMaxLength(255);
+            });
+        }
+    }
+}
---
+++ DaniaDaisy.EntityFramework/SampleDatabaseContext.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace DaniaDaisy.EntityFramework
+{
+    public partial class ProductContext : DbContext
+    {
+        private readonly string Conn;
+        public ProductContext(string conn)
+        {
+            this.Conn = conn;
+        }
+
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(this.Conn);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.SetupProductEntity();
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
